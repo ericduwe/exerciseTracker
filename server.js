5f7ce40906c5abc2b49b4e82187c02254be7c70c
Eric Duwe <ericduwe@gmail.com>
2021-09-28 23:42:10 -0500

    added app screenshots

--- server.js
+++ server.js
@@ -13,7 +13,10 @@ app.use(express.json());
 
 require("./controllers/homeRoutes")(app)
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+ });
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
