0ec022e2a45cc113c7dca2a5002b4e56ad5df727
Eric Duwe <ericduwe@gmail.com>
2021-09-28 22:19:35 -0500

    added date and totalDuration properties to workout model

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const mongoose = require("mongoose");
 const db = require("../models")
-const API = require("../public/api.js");
+
 
 
 
@@ -9,7 +9,7 @@ const API = require("../public/api.js");
 module.exports = function(app) {
 //create new exercises and add to new workout plan
 app.post("/api/workouts", ({body}, res) => {
-    db.Workout.create(body)
+    db.Workout.create({body})
     .then((dbWorkout => {
         res.json(dbWorkout);
     }))
@@ -22,9 +22,29 @@ app.post("/api/workouts", ({body}, res) => {
 app.put("/api/workouts/:id", (req, res) => {
     db.Workout.findOneAndUpdate(
         {id: req.params.id}, 
-        {$push: { exercises: req.body }}
+        {$push: { exercises: req.body }},
+        // {new: true}
     ).then(dbWorkout => {res.json(dbWorkout)})
     .catch(err => {
+        res.json(err)
+    });
+});
+
+//get last workout
+app.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.json(err);
+    })
+})
+
+app.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    }).catch(err => {
         res.json(err)
     });
 });
--- models/workout.js
+++ models/workout.js
@@ -2,6 +2,13 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
+
+    day: {
+        type: Date,
+        default: Date.now
+    },
+exercises: [
+{
   type: {
     type: String,
     trim: true,
@@ -32,6 +39,13 @@ const workoutSchema = new Schema({
     type: Number,
     required: true, 
   }
+}
+],
+
+totalDuration: {
+    type: Number
+}
+
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
